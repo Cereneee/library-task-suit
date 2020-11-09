7c616833e099370f6bfc039cc8aa7afadc3aeb78
Muha-B20 <muhammeddcakmak@outlook.com>
2020-11-08 20:03:35 -0700

    add all

--- src/test/java/com/cybertek/library/runners/CukesRunner.java
+++ src/test/java/com/cybertek/library/runners/CukesRunner.java
@@ -7,12 +7,16 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 
-        plugin = {"pretty","json:target/JSONReports/report.json","junit:target/JunitReports/report.xml",
-                "html:target/HtmlReports","html:target/cucumber-report.html"},
+        plugin = {
+               // "pretty"
+                "json:target/JSONReports/report.json"
+                ,"junit:target/JunitReports/report.xml"
+                //"html:target/HtmlReports"
+                ,"html:target/cucumber-report.html"},
         features = "src/test/resources/features",
         glue = "com/cybertek/library/step_definitions",
-        dryRun = true,
-        tags = "@book"
+        dryRun = false,
+        tags = "@librarian"
 
 )
 
